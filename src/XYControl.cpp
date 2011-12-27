e444893550d1d90e016b27ae001eaf3511027603
Damien Di Fede <dcomposer@gmail.com>
2011-12-26 17:55:49 -0800

    created an XYControl class for doing control points. hard-coded radius for the moment. removed delay and filter because they are boring.

---
+++ src/XYControl.cpp
@@ -0,0 +1,9 @@
+//
+//  XYControl.cpp
+//  melodizer
+//
+//  Created by Damien Di Fede on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/XYControl.h
@@ -0,0 +1,14 @@
+//
+//  ControlPoint.h
+//  melodizer
+//
+//  Created by Damien Di Fede on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef melodizer_ControlPoint_h
+#define melodizer_ControlPoint_h
+
+
+
+#endif
